143737eb62baddc6dbe4636880f3316232de7803
James Allen <jallen@ebi.ac.uk>
2021-09-01 15:18:36 +0100

    Adding fixtures for testing
    Test data added for a small set of species, covering a range
    of scenarios, including: multiple assemblies for one species,
    multiple genesets for one assembly, a strain group, and species
    in multiple sites/releases.
    
    Taxonomy tests and fixtures updated to be in sync with the
    metadata fixtures.

--- ncbi_taxonomy/tests/test_taxonomy.py
+++ ncbi_taxonomy/tests/test_taxonomy.py
@@ -5,27 +5,26 @@ from ncbi_taxonomy.api.utils import TaxonomyUtils
 
 class TaxonomyTest(APITestCase):
     databases = {'ncbi_taxonomy'}
-    fixtures = ['taxonomy_name', 'taxonomy_node']
+    fixtures = ['ncbi_taxonomy']
 
     def test_load_name(self):
-        self.assertEquals(TaxonomyName.objects.all().count(), 21, 'Fetched all taxonomy names')
-        taxonomy_names = TaxonomyName.objects.filter(taxon_id=10090, name_class='scientific name')
+        self.assertEquals(TaxonomyName.objects.all().count(), 355, 'Fetched all taxonomy names')
+        taxonomy_names = TaxonomyName.objects.filter(taxon_id=9771, name_class='scientific name')
         taxonomy_name = taxonomy_names[0]
-        self.assertEquals(taxonomy_name.name, 'Mus musculus', 'Correct scientific name')
+        self.assertEquals(taxonomy_name.name, 'Balaenoptera musculus', 'Correct scientific name')
 
     def test_load_node(self):
-        self.assertEquals(TaxonomyNode.objects.all().count(), 19, 'Fetched all taxonomy nodes')
-        taxonomy_node = TaxonomyUtils.fetch_node(33208)
-        self.assertEquals(taxonomy_node.parent_id, 1, 'Correct parent id')
+        self.assertEquals(TaxonomyNode.objects.all().count(), 125, 'Fetched all taxonomy nodes')
+        taxonomy_node = TaxonomyUtils.fetch_node(436486)
+        self.assertEquals(taxonomy_node.parent_id, 8492, 'Correct parent id')
         taxonomy_names = taxonomy_node.names()
         names = []
         for name in taxonomy_names:
             names.append(name.name)
-        self.assertListEqual(names, ['Animalia', 'metazoans', 'multicellular animals'], 'Correct names')
+        self.assertListEqual(names, ['dinosaur', 'dinosaurs'], 'Correct names')
 
     def test_fetch_descendant_ids(self):
-        results = TaxonomyUtils.fetch_descendent_ids('10090')
-        self.assertEqual(len(results), 17, 'Fetched all descendent nodes')
-        expected_list = [10090, 10091, 10092, 35531, 39442, 46456, 57486, 80274, 116058, 179238,
-                         477815, 477816, 947985, 1266728, 1385377, 1643390, 1879032]
+        results = TaxonomyUtils.fetch_descendent_ids('9721')
+        self.assertEqual(len(results), 5, 'Fetched all descendent nodes')
+        expected_list = [9721, 9761, 9765, 9766, 9771]
         self.assertListEqual(expected_list, results, 'Correct descendent taxon ids')
