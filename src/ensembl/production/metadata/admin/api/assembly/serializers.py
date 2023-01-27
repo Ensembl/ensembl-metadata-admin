28aec78b5ff7b96c96c1f97a56a858c3e147226b
danielp <danielp@ebi.ac.uk>
2023-01-27 14:24:57 +0000

    Fixed migrations

--- src/ensembl/production/metadata/admin/api/assembly/serializers.py
+++ src/ensembl/production/metadata/admin/api/assembly/serializers.py
@@ -1,14 +1,14 @@
-from rest_framework import serializers
-from ensembl_metadata import Assembly, AssemblySequence
-
-
-class AssemblySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Assembly
-        exclude = ['assembly_id']
-
-
-class AssemblySequenceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AssemblySequence
-        exclude = ['assembly_sequence_id', 'assembly']
+# from rest_framework import serializers
+# from ensembl.production.metadata.admi import Assembly, AssemblySequence
+#
+#
+# class AssemblySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Assembly
+#         exclude = ['assembly_id']
+#
+#
+# class AssemblySequenceSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = AssemblySequence
+#         exclude = ['assembly_sequence_id', 'assembly']
