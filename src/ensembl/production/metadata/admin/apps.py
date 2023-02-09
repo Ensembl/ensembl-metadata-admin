2ccb5f0c9ffede8ec818b75987d1a559befc9d7b
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2023-02-09 14:12:24 +0000

    Fixed some small issues with __str__ method for NoneType.

--- src/ensembl/production/metadata/admin/apps.py
+++ src/ensembl/production/metadata/admin/apps.py
@@ -17,3 +17,4 @@ from django.apps import AppConfig
 class EnsemblMetadataConfig(AppConfig):
     name = 'ensembl.production.metadata.admin'
     label = 'ensembl_metadata'
+    verbose_name = "Ensembl Metadata (MVP)"
